2ff3feb1aea2d0feb7ff576c0163cd0fb4d0ebb5
VBTX <jv.svedaite@gmail.com>
2019-07-11 12:38:08 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,11 +1,44 @@
 // Add BandInput component
 import React, { Component } from 'react'
+// import uuid from 'uuid';
+// import { connect } from 'react-redux';
 
 class BandInput extends Component {
+
+	state ={
+		name: ""
+	}
+
+
+	handleOnChange = event =>{
+		this.setState({
+			[event.target.name]:event.target.value
+		})
+
+	}
+
+
+	handleOnSubmit = event => {
+		event.preventDefault()
+		this.props.addBand(this.state);
+		this.setState({
+			name: ""
+		});
+
+
+	}
+
+
   render() {
     return(
       <div>
-        Band Input
+      <form onSubmit = {(event) => this.handleOnSubmit(event)}>
+      <input
+            type="text"
+            value={this.state.name}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
       </div>
     )
   }
